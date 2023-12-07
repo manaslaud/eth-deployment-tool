2dad6d0a1ea80c6607ebb2cf0409f7e03b10c331
manaslaud <manaslaud@hotmail.com>
2023-12-07 21:10:40 +0530

    feat: added players logic

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,8 +2,14 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+        address[] public players;
 
+
+ 
     constructor() public {
       manager=msg.sender;
+    }
+    function enter() public payable  {
+      players.push(msg.sender);
     }
 }
