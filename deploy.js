d50bf32e0bc790ce2db07cc730d353e26d296d85
manaslaud <manaslaud@hotmail.com>
2024-01-03 00:12:45 +0530

    deplyment file changes for lottery

--- deploy.js
+++ deploy.js
@@ -17,7 +17,7 @@ const deploy = async () => {
 
   try{
     const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
@@ -27,7 +27,6 @@ const deploy = async () => {
   }
   finally {
     provider.engine.stop();
-
   }
 };
 deploy();
