1871cff75c02731dc7265789b7eec41e2b490308
manaslaud <manaslaud@hotmail.com>
2024-01-09 00:37:34 +0530

    small fixes

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const inboxPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const inboxPath = path.resolve(__dirname, "contracts", "Upload.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
-
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+console.log(solc.compile(source, 1))
+module.exports = solc.compile(source, 1).contracts[":Upload"];
--- contracts/Upload.sol
+++ contracts/Upload.sol
@@ -1,49 +1,48 @@
-pragma solidity >=0.7.0 <0.9.0;
-
+pragma solidity ^0.4.17;
+pragma experimental ABIEncoderV2;
 contract Upload {
-    // constructor() {
-        
-    // }
-    struct Access {
-        address User;
-        bool access;
-    }
-    mapping (address => string[]) value;
-    mapping (address => mapping (address=>bool)) ownership;
-    mapping (address => Access[]) accessList;
-    mapping (address => mapping (address=>bool)) previousData;
+  
+  struct Access{
+     address user; 
+     bool access; //true or false
+  }
+  mapping(address=>string[]) value;
+  mapping(address=>mapping(address=>bool)) ownership;
+  mapping(address=>Access[]) accessList;
+  mapping(address=>mapping(address=>bool)) previousData;
 
-    function add(address _user,string calldata url) external{
-        value[_user].push(url);
-    }
-    function allow(address _user) external {
-        ownership[msg.sender][_user]=true;
-        if(previousData[msg.sender][_user]==true){
-            for(uint i=0; i<accessList[msg.sender].length;i++){
-                if(accessList[msg.sender][i].user==_user){
-                    accessList[msg.sender][i].access=true;
-                }
-            }
-        }
-        else{
-            accessList[msg.sender].push(Access(_user,true));
-            previousData[msg.sender][_user]==true;
-        }
+  function add(address _user,string  url) external {
+      value[_user].push(url);
+  }
+  function allow(address user) external {//def
+      ownership[msg.sender][user]=true; 
+      if(previousData[msg.sender][user]){
+         for(uint i=0;i<accessList[msg.sender].length;i++){
+             if(accessList[msg.sender][i].user==user){
+                  accessList[msg.sender][i].access=true; 
+             }
+         }
+      }else{
+          accessList[msg.sender].push(Access(user,true));  
+          previousData[msg.sender][user]=true;  
+      }
+    
+  }
+  function disallow(address user) public{
+      ownership[msg.sender][user]=false;
+      for(uint i=0;i<accessList[msg.sender].length;i++){
+          if(accessList[msg.sender][i].user==user){ 
+              accessList[msg.sender][i].access=false;  
+          }
+      }
+  }
 
-    }
-    function disallow(address _user) external {
-        ownership[msg.sender][_user]=false;
-        for(uint i=0; i<accessList[msg.sender].length;i++){
-             if(accessList[msg.sender][i].user==_user){
-                    accessList[msg.sender][i].access=false;
-                }
-        }
-        }
-        function display(address _user) external view returns(string[] memory){
-            require(_user==msg.sender || ownership[_user][msg.sender],"You do not have the access to view these files.");
-            return value[_user];
-        }
-        function shareAccess() public view returns (Access[] memory){
-            return accessList[msg.sender];
-        }
+  function display(address _user) external view returns(string[] memory){
+      require(_user==msg.sender || ownership[_user][msg.sender],"You don't have access");
+      return value[_user];
+  }
+
+  function shareAccess() public view returns(Access[] memory){
+      return accessList[msg.sender];
+  }
 }
