534aeaa4d88a3bfc9d34affd28fbaf0f3fb2bd8a
manaslaud <manaslaud@hotmail.com>
2024-01-08 23:35:55 +0530

    feat: File share contract setup

---
+++ contracts/Upload.sol
@@ -0,0 +1,43 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Upload {
+    // constructor() {
+        
+    // }
+    struct Access {
+        address User;
+        bool access;
+    }
+    mapping (address => string[]) value;
+    mapping (address => mapping (address=>bool)) ownership;
+    mapping (address => Access[]) accessList;
+    mapping (address => mapping (address=>bool)) previousData;
+
+    function add(address _user,string calldata url) external{
+        value[_user].push(url);
+    }
+    function allow(address _user) external {
+        ownership[msg.sender][_user]=true;
+        if(previousData[msg.sender][_user]==true){
+            for(uint i=0; i<accessList[msg.sender].length;i++){
+                if(accessList[msg.sender][i].user==_user){
+                    accessList[msg.sender][i].access=true;
+                }
+            }
+        }
+        else{
+            accessList[msg.sender].push(Access(_user,true));
+            previousData[msg.sender][_user]==true;
+        }
+
+    }
+    function disallow(address _user) external {
+        ownership[msg.sender][_user]=false;
+        for(uint i=0; i<accessList[msg.sender].length;i++){
+             if(accessList[msg.sender][i].user==_user){
+                    accessList[msg.sender][i].access=false;
+                }
+        }
+        }
+        
+}
